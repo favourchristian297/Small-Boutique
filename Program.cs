cfe893530d38786b3c8699b0442bed6a6dd56e45
user <user@DESKTOP-3UCD4JS>
2024-09-26 20:46:06 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿int bestkiddiesboutiqueprices =0;
+
+start:
+Console.WriteLine("welcome to best kiddies boutique");
+Console.WriteLine("POLO,Shirt,Tops,Gowns");
+int choice =int.Parse(Console.ReadLine());
+
+switch (choice)
+{
+  case 1:
+   bestkiddiesboutiqueprices += 100000;
+        Console.WriteLine("polo selected");
+        break;
+
+    case2:
+        bestkiddiesboutiqueprices += 7000;
+    Console.WriteLine("shirt selected");
+        break;
+
+        case 3:
+        bestkiddiesboutiqueprices += 8000;
+        Console.WriteLine("tops sellected");
+        break;
+
+        case 4:
+        bestkiddiesboutiqueprices += 6000;
+        Console.WriteLine("gowns selected");
+        break;
+
+        default:
+        Console.WriteLine("please input polo,shirt,tops,gowns");
+        goto start;
+        Console.WriteLine($"bestkiddiesboutiqueprices)");
+        Console.WriteLine("thanks for patronizing us");
+
+
+
+
+}
